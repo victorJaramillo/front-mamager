2521b8576ca38f54ea3162d63e1a933056181ca1
victor <victor@MacBook-Pro-de-victor-2.local>
2023-05-29 14:44:28 -0400

    new endpoint

--- src/app/header/header/header.component.ts
+++ src/app/header/header/header.component.ts
@@ -1,23 +1,43 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import { MdbCollapseDirective } from 'mdb-angular-ui-kit/collapse';
 import { Router } from '@angular/router';
+import { MdbDropdownDirective } from 'mdb-angular-ui-kit/dropdown';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
 
   @ViewChild('collapse') dropDown!: MdbCollapseDirective;
   @ViewChild('basicCollapse') basicCollapse!: MdbCollapseDirective;
+  @ViewChild('dropdownMenu') dropDownMenu!: MdbDropdownDirective;
 
   visible = false;
   showOption = false;
+  showLogin = false;
 
   colapseMenu= true;
 
+  enable_login_button:boolean= false
+  innerWidth:any
+
   constructor(private router: Router) { }
+  ngOnInit(): void {
+    if(this.innerWidth != 390){
+      this.enable_login_button = true
+    }
+  }
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.innerWidth = window.innerWidth;
+    if(this.innerWidth == 390){
+      this.enable_login_button = false
+    }else if(this.innerWidth != 390){
+      this.enable_login_button = true
+    }
+  }
 
   toggleOption(): void {
     this.showOption = !this.showOption;
@@ -25,8 +45,17 @@ export class HeaderComponent {
   toggle(){
     this.dropDown.toggle()
   }
+
+  login_btn() {
+    this.enable_login_button = !this.enable_login_button
+  }
   toggleBurger(){
+    this.login_btn()
     this.basicCollapse.toggle()
+  }
+
+  showDropdownLoginMenu() {
+    this.showLogin = !this.showLogin
   }
 
   navigateEnableServices() {
@@ -34,6 +63,7 @@ export class HeaderComponent {
     setTimeout(() => {
       this.dropDown.hide()
       this.basicCollapse.hide()
+      this.onResize()
     }, 500);
   }
   navigateApiKey() {
@@ -41,6 +71,7 @@ export class HeaderComponent {
     setTimeout(() => {
       this.dropDown.hide()
       this.basicCollapse.hide()
+      this.onResize()
     }, 500);
   }
   navigateAnime() {
@@ -48,6 +79,7 @@ export class HeaderComponent {
     setTimeout(() => {
       this.dropDown.hide()
       this.basicCollapse.hide()
+      this.onResize()
     }, 500);
   }
   navigateTodoList() {
@@ -55,6 +87,7 @@ export class HeaderComponent {
     setTimeout(() => {
       this.dropDown.hide()
       this.basicCollapse.hide()
+      this.onResize()
     }, 500);
   }
   
@@ -63,6 +96,7 @@ export class HeaderComponent {
     setTimeout(() => {
       this.dropDown.hide()
       this.basicCollapse.hide()
+      this.onResize()
     }, 500);
   }
 
