17260d09b7531c22febb4b248c7657263a2602d5
Victor Jaramillo <victor.jaramillo.gj@gmail.com>
2024-04-14 10:41:48 -0400

    validations

--- src/app/modal/modal-edit-values/modal-edit-values.component.ts
+++ src/app/modal/modal-edit-values/modal-edit-values.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
-import { MdbModalRef } from 'mdb-angular-ui-kit/modal';
+import { MdbModalRef, MdbModalService } from 'mdb-angular-ui-kit/modal';
 import { CatchResponseService } from 'src/app/services/catch-response.service';
 import { ModalEditAnimeService } from 'src/app/services/modal-edit-anime.service';
 import { UtilService } from 'src/app/services/util.service';
 import { environment as env } from 'src/environment/environment';
+import { ModalMessageComponent } from '../modal-message/modal-message.component';
+import { ModalInfoService } from 'src/app/services/modal-info.service';
 
 @Component({
   selector: 'app-modal-edit-values',
@@ -22,9 +24,14 @@ export class ModalEditValuesComponent {
 
   endpointDeleteAnime: string = 'https://nodeapi.vjdev.xyz/api/v1/animeonline/delete/'
 
+  modalMessRef: MdbModalRef<ModalMessageComponent> | null = null;
+
   constructor(public modalRef: MdbModalRef<ModalEditValuesComponent>,
+    
     private modalEditService: ModalEditAnimeService,
     private responseService: CatchResponseService,
+    private modalService: MdbModalService,
+    private modalInfoService: ModalInfoService,
     private util: UtilService){
       
     }
@@ -54,11 +61,17 @@ export class ModalEditValuesComponent {
   delete(){
     this.spinnerActive = true
     this.util.httpDeleteRequest(`${this.endpointDeleteAnime}${this.item.id}`, this.headers).subscribe( (res: any) => {
+      
       if(res.status === 'ok'){
         res.current_page = this.item.current_page
         this.responseService.setResponse(res)
         this.modalRef.close()
       }
+    }, err => {
+      this.modalInfoService.setTitel(`Error`)
+      this.modalRef.close()
+      this.modalInfoService.setBody(`${err.error.error.code}\n${err.error.error.sqlMessage}`)
+      this.modalMessRef = this.modalService.open(ModalMessageComponent)
     })
   }
 
